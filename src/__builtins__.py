61901f5c28625dcea5b4b9f0d5ecba9a74b67800
itdominator <1itdominator@gmail.com>
2021-10-10 23:33:58 -0500

    Updated __builtns__

--- src/__builtins__.py
+++ src/__builtins__.py
@@ -1,6 +1,13 @@
+import builtins
+
 class Builtins:
     def hello_world():
         print("Hello, world!")
 
+    def hello_user():
+        print("Hello, user!")
 
+
+   # NOTE: There areto ways to add global items. like so.
+    builtins.hello_user = hello_user
     __builtins__.update({"hello_world": hello_world})
