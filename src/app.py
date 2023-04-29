1e73236ee8b5049ffb3fc5531adfc9ba79acd652
itdominator <1itdominator@gmail.com>
2023-04-29 09:40:09 -0500

    Modified debug signal call setup

--- src/app.py
+++ src/app.py
@@ -5,31 +5,9 @@ import os
 # Lib imports
 
 # Application imports
+from utils.debugging import debug_signal_handler
 from utils.ipc_server import IPCServer
 from core.window import Window
-
-
-
-# Break into a Python console upon SIGUSR1 (Linux) or SIGBREAK (Windows:
-# CTRL+Pause/Break).  To be included in all production code, just in case.
-def debug_signal_handler(signal, frame):
-    del signal
-    del frame
-
-    try:
-        import rpdb2
-        logger.debug("\n\nStarting embedded RPDB2 debugger. Password is 'foobar'\n\n")
-        rpdb2.start_embedded_debugger("foobar", True, True)
-        rpdb2.setbreak(depth=1)
-        return
-    except StandardError:
-        pass
-
-    try:
-        import code
-        code.interact()
-    except StandardError as ex:
-        logger.debug(f"{ex}, returning to normal program flow...")
 
 
 
@@ -58,10 +36,11 @@ class Application(IPCServer):
                 raise AppLaunchException(f"{app_name} IPC Server Exists: Will send path(s) to it and close...")
 
         try:
+            # kill -SIGUSR2 <pid> from Linux/Unix or SIGBREAK signal from Windows
             signal.signal(
-                    vars(signal).get("SIGBREAK") or vars(signal).get("SIGUSR1"),
-                    debug_signal_handler
-                    )
+                vars(signal).get("SIGBREAK") or vars(signal).get("SIGUSR1"),
+                debug_signal_handler
+            )
         except ValueError:
             # Typically: ValueError: signal only works in main thread
             ...
